23ec9df68a2850f7374f5dc8ad6ed48e4db52946
SonTX <tranxuanson98@gmail.com>
2022-03-17 15:13:13 +0700

    init detail screen

---
+++ src/pages/invest/detail/index.tsx
@@ -0,0 +1,7 @@
+import InvestDetail from '../../../views/Invest/InvestDetails'
+
+const InvestDetailPage = () => {
+  return <InvestDetail />
+}
+
+export default InvestDetailPage
---
+++ src/views/Invest/InvestDetails/index.tsx
@@ -0,0 +1,8 @@
+import useTheme from '../../../hooks/useTheme'
+
+const InvestDetail = () => {
+  const { theme } = useTheme()
+  return <div>1231231222312231</div>
+}
+
+export default InvestDetail
