d098fd36feac2aedc6488651ec32189f1c829d11
nguyenhuudungz <nguyenhuudungz@yahoo.com>
2022-04-06 10:30:14 +0700

    Fix twitter card

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -59,10 +59,13 @@ function MyApp(props: AppProps) {
         <meta name="twitter:image" content="https://pancakeswap.finance/images/hero.png" />
         <meta
           name="twitter:description"
-          content="The most popular AMM on BSC! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by PancakeSwap), NFTs, and more, on a platform you can trust."
+          content="The most popular real estate platform on BSC! Earn MTF through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens!"
         />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:title" content="🥞 PancakeSwap - A next evolution DeFi exchange on BNB Smart Chain (BSC)" />
+        <meta
+          name="twitter:title"
+          content="Metafound - The real estate industry's digital and sharing platform for investment"
+        />
         <title>Metafound</title>
       </Head>
       <Providers store={store}>
